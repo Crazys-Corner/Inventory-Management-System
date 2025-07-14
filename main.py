d7428dc8164433f78dd5fc54a3d188d465d265f9
Crazys-Corner <craazygaming1021@gmail.com>
2025-07-14 14:17:53 -0400

    CRUD Routes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union, List
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import sqlite3
 import database
@@ -7,6 +7,7 @@ import database
 app = FastAPI()
 
 class Item(BaseModel):
+    id: int     
     name: str
     value: float
     isInUse: bool
@@ -71,3 +72,36 @@ def add_item(item: Item):
     return {"Result": "Success",
             "Message": f"Added: {item.name}"}
 
+@app.delete("/inventory/items/{item_id}")
+def delete_item(item_id: int):
+    database.cursor.execute("SELECT * FROM items WHERE id = ?", (item_id,))
+    item = database.cursor.fetchone()
+    if not item:
+        raise HTTPException(status_code=404, detail="Item not found")
+    database.cursor.execute("DELETE FROM items WHERE id = ? ", (item_id,))
+    database.conn.commit()
+    return {"result": "success", "message": f"Item with ID {item_id} deleted"}
+
+@app.put('/inventory/items/{item_id}')
+def update_item(item_id: int, item: Item):
+    database.cursor.execute(
+        "UPDATE items SET name = ?, value = ?, inuse = ?, client = ? WHERE id = ?",
+        (item.name, item.value, item.isInUse, item.client, item_id)
+    )
+    database.conn.commit()
+
+    rows = database.cursor.execute("SELECT * FROM items WHERE id = ?", (item_id,)).fetchall()
+    itemContent = [
+        {
+            "id": row[0],
+            "name": row[1],
+            "value": row[2],
+            "isInUse": bool(row[3]),
+            "client": row[4]
+        } for row in rows
+    ]
+    return {
+        "result": "success",
+        "message": f"Updated {item.name}",
+        "content": itemContent
+    }
